3149cacdec5e944fde7371c59f8d8e7f0e585423
Thomas Basham <Bashamtg@gmail.com>
2024-01-29 17:34:13 -0800

    added prop types, styled main content

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,20 @@
-import React from "react";
 import { Button } from "react-bootstrap";
+import PropTypes from "prop-types";
 
 export default function Main({ workoutData, handleButtonClick }) {
   return (
-    <main>
+    <main className="">
       <Button onClick={handleButtonClick}>Click Me</Button>
-
-      {workoutData.map((obj, index) => {
-        return <p key={index}> {obj.date.toUTCString()} </p>;
-      })}
+      <div className="border m-3 p-3" style={{ minHeight: "80vh" }}>
+        {workoutData.map((obj, index) => {
+          return <p key={index}> {obj.date.toUTCString()} </p>;
+        })}
+      </div>
     </main>
   );
 }
+
+Main.propTypes = {
+  workoutData: PropTypes.array.isRequired,
+  handleButtonClick: PropTypes.func.isRequired,
+};
